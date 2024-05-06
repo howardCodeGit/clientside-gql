0765d6e0fe6e3043adb7d899f03c21c0e5767096
Howard Sun <komugi@gmail.com>
2024-05-06 12:40:37 -0400

    Changes to be committed: 	new file:   app/gqlProvider.tsx 	modified:   app/layout.tsx 	new file:   app/signup.ts 	new file:   gql/signupMutation.ts

---
+++ gql/signupMutation.ts
@@ -0,0 +1,10 @@
+// gql/signupMutation
+import { gql } from 'urql'
+
+export const SignupMutation = gql`
+  mutation Mutation($input: AuthInput!) {
+    createUser(input: $input) {
+      token
+    }
+  }
+`
